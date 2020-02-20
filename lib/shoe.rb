888989fd2a57bb76c979b951715cbf9a59f3caee
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-20 03:33:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+BRAND = []
+
   def initialize(brand)
     @brand = brand
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+def brand=(brand)
+  @brand = brand
+  BRAND << genre 
+end
 
 end
